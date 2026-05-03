0bb211911fbf9e58320711dcb4d20809dedee49c
Oliver Kopp <kopp.dev@gmail.com>
2026-05-03 11:39:03 +0200

    Enable updating using winget - and update pips

--- update-all.bat
+++ update-all.bat
@@ -5,9 +5,15 @@ set HTTP_PROXY=
 set HTTPS_PROXY=
 echo .
 
-echo Updating all packages...
+echo Updating all winget packages...
+winget upgrade -h --all
+
+echo Updating chocolatey all packages...
 call choco upgrade all
 echo .
+
+echo Updating all pip packages
+pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pip install -U
 
 echo Updating all MiKTeX packages
 call mpm --verbose --update-db
@@ -17,4 +23,4 @@ call initexmf --mklinks --force
 echo .
 
 echo Updating texlive
-call tlmgr update --self --all --reinstall-forcibly-removed
+rem call tlmgr update --self --all --reinstall-forcibly-removed
